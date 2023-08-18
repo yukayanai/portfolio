23919e83a2d60869282fe9f593140492c169cfcf
Yuka Yanai <yukayanai@gmail.com>
2023-08-18 19:39:24 +0900

    data-module="hidenonau" removed from several pages.

--- src/javascripts/modules/index.js
+++ src/javascripts/modules/index.js
@@ -7,8 +7,13 @@ const moduleElements = document.querySelectorAll('[data-module]')
 for (var i = 0; i < moduleElements.length; i++) {
   const el = moduleElements[i]
   const name = el.getAttribute('data-module')
-  const Module = require(`./${name}`).default
-  new Module(el)
+  try {
+    const Module = require(`./${name}`).default
+    new Module(el)
+  } catch (e) {
+    // sink it
+    console.error(`Can't find module: ./${name}`);
+  }
 }
 
 /*
